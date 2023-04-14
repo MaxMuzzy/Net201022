6d4b83626db3c3d180cab176501c93c6f16197a0
Muzzy <m.a.saltykov@gmail.com>
2023-04-14 20:49:11 +0300

    HW3 is done
    Also added tmp and integrated playfab & photon in project

--- Assets/ProjectData/Scripts/Authorization.cs
+++ Assets/ProjectData/Scripts/Authorization.cs
@@ -5,40 +5,55 @@ using PlayFab.ClientModels;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Authorization : MonoBehaviourPunCallbacks
 {
     [SerializeField] private string _playFabTitle;
+    [SerializeField] private Button _logInButton;
+    [SerializeField] private TextMeshProUGUI _text;
 
     void Start()
     {
         if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
             PlayFabSettings.staticSettings.TitleId = _playFabTitle;
 
+        _logInButton.onClick.AddListener(Login);
+    }
+
+    private void Login()
+    {
         var request = new LoginWithCustomIDRequest
         {
             CustomId = "TestUser",
             CreateAccount = true
         };
-
         PlayFabClientAPI.LoginWithCustomID(request,
             result =>
             {
+                _text.color = Color.green;
+                _text.text = "Connection successful!";
                 Debug.Log(result.PlayFabId);
                 PhotonNetwork.AuthValues = new AuthenticationValues(result.PlayFabId);
                 PhotonNetwork.NickName = result.PlayFabId;
-                Connect();
+                ConnectMP();
             },
-            error => Debug.LogError(error));
+            error => 
+            {
+                _text.color = Color.red;
+                _text.text = "Error! See logs for more details.";
+                Debug.LogError(error);
+            });
     }
 
-    private void Connect()
+    private void ConnectMP()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
 
         if (PhotonNetwork.IsConnected)
         {
-            PhotonNetwork.JoinRandomOrCreateRoom(roomName: $"Room N{Random.Range(0, 9999)}");
+            return;
         }
         else
         {
@@ -46,24 +61,22 @@ public class Authorization : MonoBehaviourPunCallbacks
             PhotonNetwork.GameVersion = PhotonNetwork.AppVersion;
         }
     }
-
+    
+    
+    // Третий пункт домашки
+    // ----
+    private void DisconnectMP()
+    {
+        if (PhotonNetwork.IsConnected)
+        {   
+            PhotonNetwork.Disconnect();
+            Debug.Log("Disconnected!");
+        }
+    }
+    // ----
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
         Debug.Log("OnConnectedToMaster");
-        if (!PhotonNetwork.InRoom)
-            PhotonNetwork.JoinRandomOrCreateRoom(roomName: $"Room N{Random.Range(0, 9999)}");
-    }
-
-    public override void OnCreatedRoom()
-    {
-        base.OnCreatedRoom();
-        Debug.Log("OnCreatedRoom");
-    }
-
-    public override void OnJoinedRoom()
-    {
-        base.OnJoinedRoom();
-        Debug.Log($"OnJoinedRoom {PhotonNetwork.CurrentRoom.Name}");
     }
 }
